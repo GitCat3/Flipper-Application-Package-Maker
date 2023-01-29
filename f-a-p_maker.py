a6f42bb86a23233a5974de5c8c97b1f2dc91000e
Ethan Montellese <hulkbuster119@gmail.com>
2023-01-29 11:15:57 -0500

    added functionality for the rest of the options

--- f-a-p_maker.py
+++ f-a-p_maker.py
@@ -2,27 +2,41 @@
 
 from simple_term_menu import TerminalMenu
 from progress.spinner import Spinner
-import time
+from time import sleep
 import sys
 import subprocess
-import shutil
+from shutil import copy2
+from pathlib2 import Path
 
-options = ["Change Name", "Create"]
+options = ["Clone Flipper Zero Repo", "Change Name", "Create"]
 
 term = TerminalMenu(options)
-menu_entry_index = term.show()
-if options[menu_entry_index] == "Create":
-	p = subprocess.Popen("./fbt", cwd="../flipperzero-firmware", stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-	spinner = Spinner("Generating Flipper Application Package ")
-	while True:
-		time.sleep(0.2)
-		spinner.next()
-		poll = p.poll()
-		if poll is None:
-			continue
-		else:
-			break
+#menu_entry_index = term.show()
 
-	shutil.copy2("../flipperzero-firmware/dist/f7-D/flipper-z-f7-full-local.dfu", "firmware.dfu")
-	sys.stdout.write('\033[2K\033[1G') #Here there be dragons
-	print("finished!")
+while True:
+	menu_entry_index = term.show()
+	if options[menu_entry_index] == "Clone Flipper Zero Repo":
+		subprocess.run("git clone --recursive https://github.com/flipperdevices/flipperzero-firmware.git", shell=True)
+		copy2("flipperzero-firmware/firmware/targets/f7/furi_hal/furi_hal_version.c", "backups/furi_hal_version.c") # this is for restoring the name to otp->name because my dumb ass can't figure out how to get a line by line number and replace it
+		print("Repo has been cloned!")
+
+	elif options[menu_entry_index] == "Change Name":
+		copy2("backups/furi_hal_version.c", "flipperzero-firmware/firmware/targets/f7/furi_hal/")
+		name = input("what would you like the name of your flipper to be? ")
+		path = Path("flipperzero-firmware/firmware/targets/f7/furi_hal/furi_hal_version.c")
+		text = path.read_text()
+		text = text.replace("otp->name", f'"{name}"')
+		path.write_text(text)
+		print("Name changed!")
+
+	elif options[menu_entry_index] == "Create":
+		p = subprocess.Popen("./fbt", cwd="flipperzero-firmware", stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+		spinner = Spinner("Generating Flipper Application Package ")
+		while p.poll() is None:
+			sleep(0.2)
+			spinner.next()
+
+		copy2("flipperzero-firmware/dist/f7-D/flipper-z-f7-full-local.dfu", "firmware.dfu")
+		sys.stdout.write('\033[2K\033[1G') #Here there be dragons
+		print("Finished!")
+		break
